1386684bad02c136243d8b876adfcea8a255bb3f
Fay Salwen <fsalwen@fsalwen-ltm2.internal.salesforce.com>
2020-01-09 10:27:04 -0500

    Rearranged to put the org creation into orgInit, for the deployer.

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Define the variables
+SCRATCH_ORG_NAME="healthcloud"
+SCRATCH_ORG_DEF="config/healthcloud-scratch-def.json"
+
+# Create a scratch org
+sfdx force:org:create -f $SCRATCH_ORG_DEF -a $SCRATCH_ORG_NAME
+
 # Use the healthcloud script to create and open an org
-./healthcloud start
+./healthcloud init
